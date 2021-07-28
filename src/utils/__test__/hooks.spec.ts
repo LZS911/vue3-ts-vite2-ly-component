e56834043d5aac445028556713d8f6d14c80baf4
lizhensheng <lizhensheng@actionsky.com>
2021-07-28 19:32:44 +0800

    update

--- src/utils/__test__/hooks.spec.ts
+++ src/utils/__test__/hooks.spec.ts
@@ -1,39 +1,171 @@
 import { $ } from '..';
-import { digitalRoot, multiply } from '../codewars';
+import { digitalRoot, multiply, objToTree, addTwoNumbers, ListNode, add, judgePalindrome, myAtoi, convert, isMatch, indexAll, isSpecial } from '../codewars';
 import { useState } from '../hooks';
+import _ from 'lodash';
 
 describe('hooks.ts', () => {
-  test('number', () => {
-    const [count, setCount] = useState(0);
-    setCount($(count) + 1);
-    expect($(count)).toEqual(1);
-  });
+  //#region
+  //   test('number', () => {
+  //     const [count, setCount] = useState(0);
+  //     setCount($(count) + 1);
+  //     expect($(count)).toEqual(1);
+  //   });
 
-  test('string', () => {
-    const [str, setStr] = useState('');
-    setStr(`${$(str)}abc`);
-    expect($(str)).toEqual('abc');
-  });
+  //   test('string', () => {
+  //     const [str, setStr] = useState('');
+  //     setStr(`${$(str)}abc`);
+  //     expect($(str)).toEqual('abc');
+  //   });
 
-  test('object', () => {
-    const [obj, setObj] = useState({});
-    let tmp = { a: '1', b: '2' };
-    setObj(tmp);
-    expect($(obj)).toEqual(tmp);
-    tmp = { a: '5', b: '9' };
-    expect($(obj)).not.toEqual(tmp);
-    expect($(obj)).toEqual({ a: '1', b: '2' });
-  });
-});
+  //   test('object', () => {
+  //     const [obj, setObj] = useState({});
+  //     let tmp = { a: '1', b: '2' };
+  //     setObj(tmp);
+  //     expect($(obj)).toEqual(tmp);
+  //     tmp = { a: '5', b: '9' };
+  //     expect($(obj)).not.toEqual(tmp);
+  //     expect($(obj)).toEqual({ a: '1', b: '2' });
+  //   });
+  // });
 
-describe('codewars.ts', () => {
-  test('multiply', () => {
-    expect(multiply(1, 1)).toEqual(2);
-  });
+  // describe('codewars.ts', () => {
+  //   test('multiply', () => {
+  //     expect(multiply(1, 1)).toEqual(2);
+  //   });
 
-  test('digitalRoot', () => {
-    expect(digitalRoot(2)).toEqual(2);
-    expect(digitalRoot(16)).toEqual(7);
-    expect(digitalRoot(456)).toEqual(6);
+  //   test('digitalRoot', () => {
+  //     expect(digitalRoot(2)).toEqual(2);
+  //     expect(digitalRoot(16)).toEqual(7);
+  //     expect(digitalRoot(456)).toEqual(6);
+  //   });
+
+  //   test('objToTree', () => {
+  //     const arr = [
+  //       { id: 1, name: '部门1', pid: 0 },
+  //       { id: 2, name: '部门2', pid: 1 },
+  //       { id: 3, name: '部门3', pid: 1 },
+  //       { id: 4, name: '部门4', pid: 3 },
+  //       { id: 5, name: '部门5', pid: 4 },
+  //     ];
+  //     const res = [
+  //       {
+  //         id: 1,
+  //         name: '部门1',
+  //         pid: 0,
+  //         children: [
+  //           {
+  //             id: 2,
+  //             name: '部门2',
+  //             pid: 1,
+  //             children: []
+  //           },
+  //           {
+  //             id: 3,
+  //             name: '部门3',
+  //             pid: 1,
+  //             children: [
+  //               {
+  //                 id: 4,
+  //                 name: '部门4',
+  //                 pid: 3,
+  //                 children: [
+  //                   {
+  //                     id: 5,
+  //                     name: '部门5',
+  //                     pid: 4,
+  //                     children: []
+  //                   },
+  //                 ]
+  //               },
+  //             ]
+  //           }
+  //         ]
+  //       }
+  //     ];
+  //   });
+
+  //   // test('addTwoNumbers', () => {
+  //   //   const l1 = new ListNode();
+  //   //   const l2 = new ListNode();
+  //   //   expect(addTwoNumbers(l1, l2)).toBe([8, 9, 9, 9, 0, 0, 0, 1]);
+  //   // });
+
+  //   test('add', async () => {
+  //     const res = await add(1, 2, 3);
+  //     expect(res).toBe(6);
+  //   });
+
+  //   test('judgePalindrome', () => {
+  //     expect(judgePalindrome('abc')).toBe(false);
+  //     expect(judgePalindrome('abb')).toBe(false);
+  //     expect(judgePalindrome('ac')).toBe(false);
+  //     expect(judgePalindrome('abcd')).toBe(false);
+  //     expect(judgePalindrome('a')).toBe(true);
+  //     expect(judgePalindrome('aa')).toBe(true);
+  //     expect(judgePalindrome('bb')).toBe(true);
+  //     expect(judgePalindrome('abccba')).toBe(true);
+  //     expect(judgePalindrome('abcdcba')).toBe(true);
+  //   });
+  // test('convert', () => {
+  //   expect(convert('PAYPALISHIRING', 3)).toBe('PAHNAPLSIIGYIR');
+  //   expect(convert('A', 1)).toBe('A');
+  //   expect(convert('A', 2)).toBe('A');
+  //   expect(convert('AB', 1)).toBe('AB');
+  //   expect(convert('AB', 2)).toBe('AB');
+  //   expect(convert('ABC', 2)).toBe('ACB');
+  //   expect(convert('ABC', 3)).toBe('ABC');
+  //   expect(convert('ABCD', 2)).toBe('ACBD');
+  //   expect(convert('PAYPALISHIRING', 4)).toBe('PINALSIGYAHRPI');
+  // });
+
+  //   test('myAtoi', () => {
+  //     expect(myAtoi('42')).toBe(42);
+  //     expect(myAtoi('   42')).toBe(42);
+  //     expect(myAtoi('   -42')).toBe(-42);
+  //     expect(myAtoi('words and 987')).toBe(0);
+  //     expect(myAtoi('4193 with words')).toBe(4193);
+  //     expect(myAtoi('-91283472332')).toBe(-2147483648);
+  //     expect(myAtoi('3.14159')).toBe(3);
+  //     expect(myAtoi('-+12')).toBe(0);
+  //     expect(myAtoi('+12')).toBe(12);
+  //   });
+
+  // test('isMatch', () => {
+  //   expect(isMatch('', '')).toBe(false);
+  //   expect(isMatch('123', '')).toBe(false);
+  //   expect(isMatch('', '123')).toBe(false);
+  //   expect(isMatch('123', '1234')).toBe(false);
+  //   expect(isMatch('123', '12.')).toBe(true);
+  //   expect(isMatch('123', '...')).toBe(true);
+  //   expect(isMatch('1234', '...3')).toBe(false);
+  //   expect(isMatch('1234', '1..4')).toBe(true);
+  //   expect(isMatch('1134', '1*34')).toBe(true);
+  // });
+
+  // test('indexAll', () => {
+  //   expect(indexAll('123', '.')).toEqual([]);
+  //   expect(indexAll('123.435', '.')).toEqual([3]);
+  //   expect(indexAll('.', '.')).toEqual([0]);
+  //   expect(indexAll('1.3.4', '.')).toEqual([1, 3]);
+  //   expect(indexAll('.1.3.4', '.')).toEqual([0, 2, 4]);
+  //   expect(indexAll('.1.3.4....42.35.234.453', '.')).toEqual([0, 2, 4, 6, 7, 8, 9, 12, 15, 19]);
+  //   expect(indexAll('....', '.')).toEqual([0, 1, 2, 3]);
+  // });
+  //#endregion
+
+  test('isSpecial', () => {
+    expect(isSpecial(0)).toBe(false);
+    expect(isSpecial(1)).toBe(false);
+    expect(isSpecial(3)).toBe(false);
+    expect(isSpecial(4)).toBe(true);
+    expect(isSpecial(5)).toBe(false);
+    expect(isSpecial(9)).toBe(true);
+    expect(isSpecial(10)).toBe(false);
+    expect(isSpecial(40)).toBe(true);
+    expect(isSpecial(50)).toBe(false);
+    expect(isSpecial(90)).toBe(true);
+    expect(isSpecial(400)).toBe(true);
+    expect(isSpecial(500)).toBe(false);
+    expect(isSpecial(900)).toBe(true);
   });
 });
--- src/utils/codewars.ts
+++ src/utils/codewars.ts
@@ -1,3 +1,15 @@
+/* eslint-disable no-loop-func */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-restricted-properties */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-empty-function */
+/* eslint-disable-next-line no-useless-escape */
+import _ from 'lodash';
+
+export const MAX_NUMBER = Math.pow(2, 31) - 1;
+export const MIN_NUMBER = Math.pow(-2, 31);
 export function multiply(a: number, b: number): number {
   return a + b;
 }
@@ -10,3 +22,225 @@ export const digitalRoot = (n: number): number => {
 
   return digitalRoot(sum);
 };
+
+interface IObjToTree {
+  id: number;
+  name: string;
+  pid: number;
+  children?: IObjToTree[]
+}
+
+export const objToTree = (arr: IObjToTree[]) => {
+  const res: any[] = [];
+  for (let i = 0; i < arr.length; ++i) {
+    for (let j = i + 1; j < arr.length; ++j) {
+      if (arr[i].id === arr[j].pid) {
+        if (!!res[i]?.children) {
+          res[i].children = arr[j];
+        } else {
+          res.push({ ...arr[i], children: arr[j] });
+        }
+      }
+    }
+  }
+
+  return res;
+};
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+export class ListNode {
+  val: number
+
+  next: ListNode | null
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+  }
+}
+export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  const res = new ListNode();
+  let copy = res;
+  let addFlag = 0;
+  while (l1 || l2 || addFlag) {
+    let sum = 0;
+    const tmp = (l1?.val ?? 0) + (l2?.val ?? 0);
+    if (tmp >= 10) {
+      sum = addFlag ? (addFlag + tmp) % 10 : tmp % 10;
+      addFlag = parseInt(`${tmp / 10}`);
+    } else {
+      sum = addFlag ? addFlag + tmp : tmp;
+      addFlag = 0;
+    }
+    if (sum >= 10) {
+      addFlag = parseInt(`${sum / 10}`);
+      sum %= 10;
+    }
+    const sumNode = new ListNode(sum);
+    copy.next = sumNode;
+    copy = sumNode;
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
+  }
+
+  return res.next;
+}
+
+const addRemote = async (a: number, b: number) => new Promise<number>((resolve) => {
+  setTimeout(() => resolve(a + b), 1000);
+});
+
+export async function add(...inputs: number[]) {
+  if (inputs.length === 0) {
+    return 0;
+  }
+  if (inputs.length === 1) {
+    return inputs[0];
+  }
+
+  let res = 0;
+  for (const val of inputs) {
+    res = await addRemote(val, res);
+  }
+
+  return res;
+}
+
+//abcdcba.  abccba.
+export function judgePalindrome(target: string) {
+  if (target.length === 1) return true;
+  const len = target.length;
+  let j = len - 1;
+  for (let i = 0; i < len / 2; ++i) {
+    if (target[i] !== target[j]) {
+      break;
+    }
+    --j;
+  }
+  if (j > len / 2 - 1) {
+    return false;
+  }
+  return true;
+}
+export function convert(target: string, numRows: number): string {
+  const len = target.length;
+  if (len <= 2 || numRows === 1 || len <= numRows) return target;
+  let res = '';
+  const map = new Map<{ x: number, y: number }, string>();
+  let i = 0;
+
+  //计算总共横向数量
+  const xSum = (len / (numRows + numRows - 2)) * (numRows - 1);
+  for (let x = 0; x < xSum && i < len; ++x) {
+    for (let y = 0; y < numRows; ++y) {
+      if (x % (numRows - 1) && x !== 0 && numRows > 2) {
+        //只push一次
+        map.set({ x, y: numRows - 1 - (x % (numRows - 1)) }, target[i]);
+        ++i;
+        break;
+      }
+      map.set({ x, y }, target[i]);
+      ++i;
+    }
+  }
+  for (let i = 0; i < numRows; i++) {
+    map.forEach((val, key) => {
+      if (key.y === i) {
+        val = val ?? '';
+        res += val;
+      }
+    });
+  }
+
+  return res;
+}
+
+export function myAtoi(target: string): number {
+  ///^[\+\-]?\d+/  好好学正则吧 ...
+  const str = target.trim().match(/^\+?[\-|0-9][0-9]*/);
+  const res = str ? +str[0] ? +str[0] : 0 : 0;
+  if (res < MIN_NUMBER) return MIN_NUMBER;
+  if (res > MAX_NUMBER) return MAX_NUMBER;
+  return res;
+}
+
+/**
+ *   . 匹配任意单个字符
+ *   * 匹配零个或多个前面的那一个元素
+ * @param target
+ * @param validate
+ */
+export function isMatch(target: string, validate: string): boolean {
+  const _x = '*';
+  const _d = '.';
+  if ((!target && !validate) || (!target && validate) || (target && !validate)) return false;
+  if (target !== validate && !validate.includes(_x) && !validate.includes(_d)) return false;
+  if (validate === _d + _x) return true;
+
+  // 只有 . 的情况
+  if (validate.includes(_d) && !validate.includes(_x)) {
+    if (target.length !== validate.length) return false;
+    const dIndexArr = indexAll(validate, _d);
+    const checkArr: number[] = [];
+    for (let i = 0; i < target.length; ++i) {
+      if (target[i] !== validate[i]) {
+        checkArr.push(i);
+      }
+    }
+    if (!_.isEqual(dIndexArr, checkArr)) return false;
+  }
+
+  //只有 * 的情况  target = "aab" validate = "c*a*b"
+  if (!validate.includes(_d) && validate.includes(_x)) {
+    //拿到 * 前面那个字符的index
+    const xIndexArr = indexAll(validate, _x).map((i) => i - 1);
+    if (xIndexArr.some((i) => i < 0)) throw Error(' * error');
+
+    for (let i = 0; i < target.length; ++i) {
+      if (xIndexArr.includes(i)) {
+        //当前 validate_[i] 为带有 * 的字符
+      } else if (target[i] !== validate[i]) return false;
+    }
+  }
+
+  return true;
+}
+
+export function indexAll(params: string, target: string) {
+  const res: number[] = [];
+  let index = 0;
+  for (let i = 0; params.includes(target); ++i) {
+    index += params.indexOf(target) + (i ? 1 : 0);
+    res.push(index);
+    params = params.slice(params.indexOf(target) + 1);
+  }
+
+  return res;
+}
+
+export function isSpecial(num: number) {
+  if ((num > 10 && num % 10)) return false;
+  while (num > 10) {
+    num /= 10;
+  }
+
+  if (10 - num === 1 || 5 - num === 1) return true;
+  return false;
+}
+
+export function intToRoman(num: number): string {
+  const res = '';
+  if (num < 1) return '';
+
+  return res;
+}
