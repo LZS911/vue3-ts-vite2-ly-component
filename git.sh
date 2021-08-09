855d6cbbe03593e8773219c03fc78c551db36e18
lizhensheng <lizhensheng@actionsky.com>
2021-08-09 19:00:22 +0800

    update

--- git.sh
+++ git.sh
@@ -5,6 +5,20 @@ then
   git commit -m $2
   git push
   echo '代码提交成功'
-else
+elif [ $1 == 'pull' ]
+then
+  git pull
   echo '代码拉取成功'
+elif [ $1 == 'stash' ]
+then
+  git stash save $2
+  echo '提交暂存成功'
+elif [ $1 == 'pop' ]
+then
+  git stash pop $2
+  echo '提交弹出成功'
+elif [ $1 == 'apply' ]
+then
+  git stash apply $2
+  echo '提交恢复成功'
 fi
