5ec7dd6584556a131968ad81f1ad70b06aa77b12
lizhensheng <lizhensheng@actionsky.com>
2021-08-09 22:35:13 +0800

    update

--- src/utils/codewars.ts
+++ src/utils/codewars.ts
@@ -939,3 +939,44 @@ export function findSubstring(s: string, words: string[]): number[] {
   // }
   return res;
 }
+
+/**
+ * 找出这个数组排序出的所有数中，刚好比当前数大的那个数
+
+比如当前 nums = [1,2,3]。这个数是123，找出1，2，3这3个数字排序可能的所有数，排序后，比123大的那个数 也就是132
+
+如果当前 nums = [3,2,1]。这就是1，2，3所有排序中最大的那个数，那么就返回1，2，3排序后所有数中最小的那个，也就是1，2，3 -> [1,2,3]
+
+[1, 4, 3, 5] => 1 4 5 3
+[1, 4, 5, 3] => 1 5 4 3
+ * @param nums
+ */
+export function nextPermutation(nums: number[]): void {
+  let flag = true;
+  for (let i = nums.length - 1; i > 0; --i) {
+    if (nums[i - 1] < nums[i] && flag) {
+      flag = false;
+      let min = Math.min(...nums.slice(i, nums.length).filter((item) => item > nums[i - 1]));
+      if (nums[i - 1] < min) {
+        let minIndexOf = nums.slice(i, nums.length).indexOf(min) + i;
+        let tmp = nums[i - 1];
+        nums[i - 1] = nums[minIndexOf];
+        nums[minIndexOf] = tmp;
+      } else {
+        let tmp = nums[i - 1];
+        nums[i - 1] = nums[i];
+        nums[i] = tmp;
+      }
+      for (let j = 0; j < nums.length - i - 1; j++) {
+        for (let k = i; k < nums.length - j - 1; k++) {
+          if (nums[k] > nums[k + 1]) {
+            let tmp = nums[k];
+            nums[k] = nums[k + 1];
+            nums[k + 1] = tmp;
+          }
+        }
+      }
+    }
+  }
+  if (flag) nums.sort((a, b) => a - b);
+}
